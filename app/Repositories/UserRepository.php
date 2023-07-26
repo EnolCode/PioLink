fc8dabac08844c400b9f6955ebb545bf5e067c79
EnolIgareta <enol1code@gmail.com>
2023-07-26 22:25:17 +0200

    profile deleted when user is delete

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -5,6 +5,7 @@ namespace App\Repositories;
 use App\Http\Requests\UserUpdatedRequest;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
 
 class UserRepository extends BaseRepository
 {
@@ -13,16 +14,17 @@ class UserRepository extends BaseRepository
     ];
     public function __construct(User $user)
     {
-        parent::__construct($user);
-    }
-
-    public function all(): Collection
-    {
-        return $this->model->with(self::RELATIONS)->get();
+        parent::__construct($user, self::RELATIONS);
     }
 
     public function findUserByUsername(string $name): User|null
     {
         return User::where('name', $name)->first();
+    }
+
+    public function delete(Model $model): void
+    {
+        $model->profile->delete();
+        $model->delete();
     }
 }
