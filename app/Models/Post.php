4845c4640be2c76a7ab0ff09f58a122566b6833e
EnolIgareta <enol1code@gmail.com>
2023-07-27 21:46:54 +0200

    post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+    protected $hidden = ['created_at', 'updated_at'];
+
+    public function profile(): BelongsTo
+    {
+        return $this->belongsTo(Profile::class);
+    }
 }
