34a7c0e87ee8100354febd3db32a2baaade28433
EnolIgareta <enol1code@gmail.com>
2023-07-30 10:00:06 +0200

    postRepository

---
+++ app/Repositories/PostRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Post;
+
+class PostRepository extends BaseRepository
+{
+
+    public function __construct(Post $post)
+    {
+        parent::__construct($post);
+    }
+}
